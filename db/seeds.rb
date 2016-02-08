00197f3be815d6dd053e54f51cbc8b0c808b870e
ed-karabinus <ejkarabin@gmail.com>
2016-02-08 02:18:02 -0600

    Finished Intro to Rake Codealong

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@ Student.create(name: "April", grade: "10th")
 Student.create(name: "Luke", grade: "9th")
 Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
+
+# This "seed" data will be loaded into our database if we run the command
+# "rake db:seed" after running the command "rake db:migrate" at the terminal.
