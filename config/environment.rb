1e4f8991a7f218759da8959bbca6f17b3a8db1c3
agdavid <antonio.g.david@gmail.com>
2016-03-18 14:10:23 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,13 @@
 require 'sqlite3'
 require 'pry'
+require 'require_all'
+
+def reload!
+  require_all './lib/student.rb'
+end
 
 require_relative "../lib/student.rb"
+
+puts "New is loaded!"
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
