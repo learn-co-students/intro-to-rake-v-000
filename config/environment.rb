12edfae5863ad6663ea5121942e753b14ac4cce2
Judy McGoldrick <jjeanius@verizon.net>
2018-05-11 23:18:58 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,4 @@ require 'pry'
 
 require_relative "../lib/student.rb"
 
-DB = {:conn => SQLite3::Database.new("db/students.db")}
-
-task :environment do
-  require_relative './config/environment'
-end
+DB = {:conn => SQLite3::Database.new("db/student.db")}
